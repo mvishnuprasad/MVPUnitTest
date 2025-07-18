38614efd6e888b9b4e2bb5165d9acabce668fc18
vishnuprasad <vpm735@gmail.com>
2025-07-18 20:30:52 +0530

    initial unit tests added

--- MVPUnitTestTests/Models/SignUpFormValidatorTests.swift
+++ MVPUnitTestTests/Models/SignUpFormValidatorTests.swift
@@ -26,26 +26,23 @@ final class SignInFormModelValidateTests: XCTestCase {
         //Arrange
         // Act
         
-        let isFirstNameValid = sut.IsValidFirstName("oday")
+        let isFirstNameValid = sut.IsValidFirstName("vishnu")
         //Assert
-        XCTAssertTrue(isFirstNameValid,"the IsValidFirstName() should return true if first name is valid but it's return false")
+        XCTAssertTrue(isFirstNameValid,"the IsValidFirstName() should return true if first name is valid else return false")
     }
     
     func testSignUpFormModelValidateTests_FirstNameIsVeryShort_ShouldReturnFalse(){
-        //Arrange
-        //Act
-        
-        let isFirstNameVeryShort = sut.IsValidFirstName("mo")
+        let isFirstNameVeryShort = sut.IsValidFirstName("vp")
         
         // Assert
-        XCTAssertFalse(isFirstNameVeryShort,"the IsValidFirstName() should return false if first name is less than (\(SignUpConstants.firstNameMinLength) char but it return true ")
+        XCTAssertFalse(isFirstNameVeryShort,"the IsValidFirstName() should return false if first name is less than (\(SignUpConstants.firstNameMinLength) else it return true ")
     }
     
     func testSignUpFormModelTests_FirstNameIsTooLong_ShouldReturnFalse(){
         //Arrange
         
         //Act
-        let isShortFirstName = sut.IsValidFirstName("odayMohammedAbdelsalam")
+        let isShortFirstName = sut.IsValidFirstName("vishnuprasadmvishnuprasadm")
         
         XCTAssertFalse(isShortFirstName,"the IsValidFirstName() should return false if first name is too long and greater than (\(SignUpConstants.firstNameMaxLength) char but it return true ")
     }
@@ -53,37 +50,33 @@ final class SignInFormModelValidateTests: XCTestCase {
     func testSignUpFormModelTests_PasswordIsValid_ShouldReturnTrue(){
         //Arrange
         //Act
-        let isValidPassword = sut.isValidPassword(password: "odayMohammed")
+        let isValidPassword = sut.isValidPassword(password: "vishnuprasad")
         // Assert
         XCTAssertTrue(isValidPassword,"the isValidPassword() should return true if password i passed is greater than (\(SignUpConstants.minPasswordLength)) char but it return false ")
         
     }
     
     func testSignUpFormModelTests_PasswordIsTooShort_ShouldReturnFalse(){
-        let isValidPassword = sut.isValidPassword(password: "oday")
+        let isValidPassword = sut.isValidPassword(password: "vpm")
         XCTAssertFalse(isValidPassword,"the isValidPassword() should return false if password i passed is less than (\(SignUpConstants.minPasswordLength)) char but it return true ")
         
     }
     
     func testSignUpFormModelTests_lastNameIsValid_ShouldReturnTrue(){
        
-        let isValidLastName = sut.isValidLastNme(lastName: "oday")
+        let isValidLastName = sut.isValidLastNme(lastName: "prasad")
         
         XCTAssertTrue(isValidLastName, "the isValidLastNme() should return true if last name is valid  but it return false ")
     }
     
     func testSignUpFormModelTests_lastNameIstooShort_ShouldReturnFalse(){
        
-        let isValidLastName = sut.isValidLastNme(lastName: "od")
+        let isValidLastName = sut.isValidLastNme(lastName: "vp")
         XCTAssertFalse(isValidLastName, "the isValidLastNme() should return false if password i passed is less than (\(SignUpConstants.minPasswordLength)) char but it return true ")
     }
     
     func testSignUpFormModelTests_lastNameIstooLong_ShouldReturnFalse(){
-        //Arrange
-        
-        //Act
-        let isValidLastName = sut.isValidLastNme(lastName: "odymojdhjdshjnfjshfjhsfnbfsjbsafgjhgfjhagshfbsnbhsfghgfddbfdkfhjkd")
-        //ASsert
+        let isValidLastName = sut.isValidLastNme(lastName: "prasadmvishnu")
         XCTAssertFalse(isValidLastName, "the isValidLastNme() should return false if password i passed is less than (\(SignUpConstants.firstNameMaxLength)) char but it return true ")
     }
     
@@ -94,20 +87,15 @@ final class SignInFormModelValidateTests: XCTestCase {
         //Act
         let IsMatched = sut.IsPasswordMatched(password:"123456",Repeated:"123456")
         
-        //Assert
-        
         XCTAssertTrue(IsMatched, "the IsPasswordMatched() should return true if password match but it return false ")
         
     }
     
     func testSignUpFormModelTests_passwordIsNotMatched_ShouldReturnFalse() {
-        
-        //Arrange
-        
-        //Act
+       
         let IsMatched = sut.IsPasswordMatched(password:"123456",Repeated:"1234")
         
-        //Assert
+        
         
         XCTAssertFalse(IsMatched, "the IsPasswordMatched() should return false if password not match but it return true ")
         
